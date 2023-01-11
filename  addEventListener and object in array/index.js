0feffa62a3fd8b22414e61b57a11ed793da63f45
airp0wer <mgyp@mail.ru>
2023-01-11 16:46:27 +0300

    from lenovo

---
+++  addEventListener and object in array/index.js
@@ -0,0 +1,22 @@
+const btnEl = document.getElementById('btn-el');
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    }, 
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+btnEl.addEventListener("click", function () {
+
+    console.log(data[0].score);
+})
+
+
+
+// Fetch the button from the DOM, store it in a variable
+// Use addEventListener() to listen for button clicks
+// Log Jane's score when the button is clicked (via data)
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-function getFerts(arr) {
-    return arr[arr]
-}
+let abc = localStorage.setItem("1");
+
+console.log(abc);
+
+console.log(localStorage.getItem("1"));
